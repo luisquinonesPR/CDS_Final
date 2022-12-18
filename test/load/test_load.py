ed92e7e96b333637918f532ce70a0048383b8bd8
danidlsa <danidlsa@hotmail.com>
2022-12-18 20:52:36 +0100

    load test working

--- test/load/test_load.py
+++ test/load/test_load.py
@@ -10,9 +10,8 @@ class Test_Load(unittest.TestCase):
     def test_Load(self):
         expected_output = pd.read_csv("Flights_Data.csv")
         output = load.Load('Flights_Data.csv')
-        assert_frame_equal(output, expected_output, check_dtype=False)
-        with self.assertRaises(FileNotFoundError):
-            load.Load('whatever.csv')
+        output = output.df
+        assert_frame_equal(expected_output, output, check_dtype=False)
 
 
 
