6a57e0066b02a5aa792ec7ae560ab1530cc79a89
Margherita-IDEMS <91319540+MargheritaPhilipp@users.noreply.github.com>
2022-12-20 17:02:43 +0100

    :construction_worker: dataframe type test
    tried assertIsInstance

--- test/split/test_split.py
+++ test/split/test_split.py
@@ -10,7 +10,7 @@ from pandas.testing import assert_frame_equal
 
 class TestSplit(unittest.TestCase):
     
-    def test_split(self):        
+    def test_split_size(self):        
         data = {'Col1': ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'],
                 'Col2': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
         df_input = pd.DataFrame(data)
@@ -20,3 +20,13 @@ class TestSplit(unittest.TestCase):
 
         expected_output = 2 #or whatever fraction of 10 we end up defining as the test
         self.assertEqual(len(test_output), expected_output)
+
+    def test_split_type(self):        
+        data = {'Col1': ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'],
+                'Col2': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
+        df_input = pd.DataFrame(data)
+        df_output = Split(df_input)
+        test_output = df_output.test
+        train_output = df_output.train
+
+        self.assertIsInstance(train_output, pandas.core.frame.DataFrame)
